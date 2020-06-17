d1333fc6add04ee9143b9544ca62c7cca1f100bd
Dylan Lott <lott.dylan@gmail.com>
2020-06-16 20:21:36 -0600

    WIP adds more of createGame flow

--- frontend/src/components/Games.vue
+++ frontend/src/components/Games.vue
@@ -1,41 +1,52 @@
 <template>
-  <div class="games" v-if="gameID == ''">
-    <h1>Welcome, {{ username }}</h1>
+  <div class="games shell container is-fluid" v-if="gameID == ''">
+    <div class="columns">
+      <div class="column">
+        <h1 class="title is-1">Welcome, {{ username }}</h1>
+      </div>
+    </div>
     <hr>
     <div class="hero">
       <div class="container">
-        <b-field label="Pick your Commander.">
-          <b-input v-model="deck.commander"></b-input>
-        </b-field>
-        <h3>Add the 99.</h3>
-          <p>Note: There must be exactly 99 cards in this list, they need to be spelled exactly correct,
-            and there can't be duplicates. Standard EDH rules and banlist applies. We recommend using
-            <a href="www.archidekt.com">Archidekt</a> to generate your decklists so that spelling errors
-            and quantities aren't an issue.
-          </p>
-          <div>
+        <div class="columns">
+          <div class="column">
+            <p class="title is-4">Pick your commander</p>
+            <!-- TODO: Make this an autocomplete that pulls from a list of all commanders  -->
+            <b-input v-model="deck.commander"></b-input>
+            <br></br>
+            <h3 class="title is-4">Add the 99.</h3>
+            <p>Note: There must be exactly 99 cards in this list, they need to be spelled exactly correct,
+              and there can't be duplicates. <br></br>
+              Standard EDH rules and banlist applies. 
+            </p>
+            <p>
+              We recommend using
+              <a href="www.archidekt.com">Archidekt</a> to generate your decklists so that spelling errors
+              and quantities aren't an issue.
+            </p>
             <b-field label="Add the other 99 cards.">
-              <b-input maxlength="200" v-model="deck.library" type="textarea"></b-input>
+              <b-input maxlength="200000" v-model="deck.library" type="textarea"></b-input>
             </b-field>
           </div>
-          <b-button @click="handleCreateGame()"
-          type="button"
-          class="is-success">Start a new game</b-button>
+        </div>
+        <b-button @click="handleCreateGame()" type="button" class="is-success">Start a new game</b-button>
       </div>
     </div>
 
     <div class="container">
       <h2>Or join an existing game</h2>
       <p>Enter the game code below and your deck and you'll be
-      joined into the game</p>
+        joined into the game</p>
       <div class="">
-        <b-input type="text"
-        v-model="joinGameID"
-        placeholder="Game ID"></b-input>
+        <b-input type="text" v-model="joinGameID" placeholder="Game ID"></b-input>
       </div>
-      <b-button @click="handleJoinGame()"
-      type="button"
-      class="is-primary">Join Game</b-button>
+      <b-button @click="handleJoinGame()" type="button" class="is-primary">Join Game</b-button>
+    </div>
+
+    <div class="columns">
+      <div class="column">
+        <code>{{ deck }}</code>
+      </div>
     </div>
   </div>
 </template>
@@ -53,7 +64,7 @@ export default {
       deck: {
         library: '',
         commander: ''
-      }
+      },
     }
   },
   beforeRouteEnter(to, from, next) {
@@ -64,7 +75,6 @@ export default {
     });
   },
   apollo: {
-
   },
   computed: {
     players () {
@@ -83,24 +93,12 @@ export default {
   },
   methods: {
     handleCreateGame() {
+      console.log('creating game with deck: ', this.deck)
       this.$apollo.mutate({
-        mutation: gql`mutation {
-          createGame(input: {
-            Players:[
-              {
-                Deck: {
-                  name: "Karlov",
-                  commander:"Karlov of the Ghost Council",
-                  cards: [
-                    "Teysa, Envoy of Ghosts"
-                  ]
-                },
-                Username: "shakezula"
-              }
-            ]
-          }){
+        mutation: gql`mutation ($input: InputGame!) {
+          createGame(inputGame: $input){
            	id
-            created_at3
+            created_at
         		players {
               GameID
               Commander {
@@ -111,14 +109,22 @@ export default {
         }`,
         variables: {
           inputGame: {
-            players: [{
-              Deck: [],
-              Username: 'shakezula'
+            Players: [{
+              Username: 'shakezula',
+              Commander: [ ...this.deck.commander ],
+              Library: [ ...this.deck.library ]
             }]
           }
         }
       })
-      router.push({ path: '/games/1234' })
+      .then((data) => {
+        console.log('GOT DATA!!!: ', data)
+        $id = '1234' // TODO get id from response $gameID 
+        router.push({ path: `/games/${id}` })
+      })
+      .catch((err) => {
+        console.error('got error back: ', err)
+      })
     },
     handleJoinGame() {
       // this.$apollo.mutate({
@@ -128,8 +134,5 @@ export default {
       router.push({ name: 'board', params: { id: this.joinGameID }})
     }
   },
-  apollo: {
-
-  }
 }
 </script>
