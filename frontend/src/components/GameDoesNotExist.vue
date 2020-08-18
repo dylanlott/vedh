ad77ae9cd813b9fb0dae985902a63f7de3a6f4cf
Dylan Lott <lott.dylan@gmail.com>
2020-08-18 17:48:19 -0600

    Adds game does not exist page

---
+++ frontend/src/components/GameDoesNotExist.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="columns level">
+      <div class="column level-items has-text-centered">
+        <h1 class="title is-centered">Game does not exist.</h1>
+        <h3><i class="far fa-exclamation-square"></i></h3>
+      </div>  
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameDoesNotExist'
+}
+</script>
+
+<style>
+
+</style>
