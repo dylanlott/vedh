930d48807d31d37d0b183ec738c510ca31f1aeaa
Dylan Lott <lott.dylan@gmail.com>
2020-12-31 12:44:52 -0700

    comment out implementation #1 tests

--- server/translator_test.go
+++ server/translator_test.go
@@ -1,43 +1,39 @@
 package server
 
-import (
-	"testing"
+// # Implementation 1 test
+// This didn't end up working out like I thought.
+// func TestInputGameTranslator(t *testing.T) {
+// 	var cases = []struct {
+// 		name string
+// 		from *InputGame
+// 		want interface{}
+// 	}{
+// 		{
+// 			// just to skeletonize and set things up
+// 			name: "test translating empty input game",
+// 			from: &InputGame{
+// 				ID: "test123",
+// 			},
+// 			want: Game{
+// 				ID:     "test123",
+// 				Handle: nil,
+// 			},
+// 		},
+// 	}
 
-	"github.com/google/go-cmp/cmp"
-)
+// 	for _, tt := range cases {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			to := Game{}
+// 			poly := &polyglot{}
+// 			err := poly.Translate(&to, tt.from, InputGameTranslator)
+// 			if err != nil {
+// 				t.Errorf("failed to translate: %+v\n", err)
+// 			}
 
-func TestInputGameTranslator(t *testing.T) {
-	var cases = []struct {
-		name string
-		from *InputGame
-		want interface{}
-	}{
-		{
-			// just to skeletonize and set things up
-			name: "test translating empty input game",
-			from: &InputGame{
-				ID: "test123",
-			},
-			want: Game{
-				ID:     "test123",
-				Handle: nil,
-			},
-		},
-	}
-
-	for _, tt := range cases {
-		t.Run(tt.name, func(t *testing.T) {
-			to := Game{}
-			poly := &polyglot{}
-			err := poly.Translate(&to, tt.from, InputGameTranslator)
-			if err != nil {
-				t.Errorf("failed to translate: %+v\n", err)
-			}
-
-			t.Logf("got here: %+v", to)
-			if diff := cmp.Diff(tt.want, to); diff != "" {
-				t.Errorf("failed to translate: %+v\n", diff)
-			}
-		})
-	}
-}
+// 			t.Logf("got here: %+v", to)
+// 			if diff := cmp.Diff(tt.want, to); diff != "" {
+// 				t.Errorf("failed to translate: %+v\n", diff)
+// 			}
+// 		})
+// 	}
+// }
