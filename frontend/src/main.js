b23eb2e0b627aff1c859fe5047c4394063bcf561
Dylan Lott <lott.dylan@gmail.com>
2021-03-22 16:02:31 -0600

    refactors main.js to put apollo client in gqlclient

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,59 +1,25 @@
-import Vue from 'vue';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import VueApollo from 'vue-apollo';
-import { split } from 'apollo-link';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
-
-// Buefy
+import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+import api from '@/gqlclient'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
-import './scss/custom.scss';
-Vue.use(Buefy)
-
-// VueCookies
+import './scss/custom.scss'
 import VueCookies from 'vue-cookies'
+import router from './router'
+import App from './App.vue'
+import store from './store'
+import { AuthPlugin } from './auth'
+
+Vue.use(Buefy)
 Vue.use(VueCookies)
 Vue.$cookies.config('7d')
+Vue.config.productionTip = false
 
-import router from './router';
-import App from './App.vue';
-import store from './store';
-import { AuthPlugin } from './auth';
-
-Vue.config.productionTip = false;
-
-const httpLink = new HttpLink({
-  uri: 'http://localhost:8080/graphql',
-});
-const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:8080/graphql',
-  options: {
-    reconnect: true,
-  },
-});
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
-  },
-  wsLink,
-  httpLink,
-);
-const apolloClient = new ApolloClient({
-  link: link,
-  cache: new InMemoryCache({
-    addTypename: false
-  }),
-});
 const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
+  defaultClient: api,
 });
-
-Vue.use(VueApollo);
-Vue.use(AuthPlugin);
+Vue.use(VueApollo)
+Vue.use(AuthPlugin)
 
 const vm = new Vue({
   router,
