dd2a56985d9247bc522d0f68381fa76b85c28cc5
dylan_lott <lott.dylan@gmail.com>
2021-07-05 17:38:47 -0700

    adds logout function to navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -16,7 +16,7 @@
 
         <template slot="end">
             <b-navbar-item tag="div">
-                <div class="buttons">
+                <div class="buttons" v-if="!user.Token">
                     <a href="/signup" class="button is-primary">
                         <strong>Sign up</strong>
                     </a>
@@ -26,15 +26,36 @@
                         Log in
                     </a>
                 </div>
+                <div v-else>
+                    <!-- ### TODO: Create the Profile page ###
+                    <a tag="router-link"
+                    href="/profile"
+                    class="button is-dark">
+                       Profile 
+                    </a> -->
+                    <a tag="router-link"
+                    href="#"
+                    @click="handleLogout()"
+                    class="button is-light">
+                       Log Out 
+                    </a>
+                </div>
             </b-navbar-item>
         </template>
     </b-navbar>
 </template>
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'navbar',
-  data () {
-    return {}
+  computed: mapState({
+      user: state => state.User.User
+  }),
+  methods: {
+      handleLogout() {
+          this.$store.dispatch('logout')
+      }
   }
 }
 </script>
