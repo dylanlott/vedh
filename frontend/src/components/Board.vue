f0c5d28be17f1b54002c31279b3e7d60e4e28c43
dylan_lott <lott.dylan@gmail.com>
2021-07-09 02:01:51 -0700

    Working on toolbar and player board controls

--- frontend/src/components/Board.vue
+++ frontend/src/components/Board.vue
@@ -1,76 +1,118 @@
 <template>
-  <div class="board shell" v-if="user && game">
+  <div class="board container is-fluid" v-if="user && game">
     <!-- TURN TRACKER -->
-    <div class="shell column">
+    <div class="column">
       <TurnTracker :game="game" />
     </div>
-    <div class="columns shell" :key="player.ID" v-for="player in game.PlayerIDs">
+    <div class="columns" :key="player.ID" v-for="player in game.PlayerIDs">
       <!-- ### OPPONENTS BOARDSTATES ### -->
-      <div class="column" v-if="user.ID && player.ID != user.ID">
+      <div class="columns" v-if="user.ID && player.ID != user.ID">
         <div class="title">{{ player.Username }} {{ player.ID }}</div>
       </div>
     </div>
-    <div class="columns shell">
-      <!-- ### END OF OPPONENTS BOARDSTATES ### -->
+    <!-- ### END OF OPPONENTS BOARDSTATES ### -->
 
-      <!-- ### SELF BOARDSTATE ### -->
-      <div class="column" v-if="self">
-        <div class="title is-6">{{ user.Username }} {{ user.ID }}</div>
+    <div class="title">{{ user.Username }}</div>
+    <!-- ### SELF BOARDSTATE - PUBLIC SECTION ### -->
+    <div class="columns" v-if="self">
+      <!-- SELF - BATTLEFIELD -->
+      <div class="column">
+        <p class="title is-5">Battlefield</p>
+        <draggable
+          class="columns is-mobile"
+          @change="handleChange()"
+          v-model="self.Field"
+          group="people"
+          @start="drag = true"
+          @end="drag = false"
+        >
+          <!-- Note: Cards can only be tapped on the battlefield -->
+          <div @click="handleTap(card)" class="column mtg-card" v-for="card in self.Field" :key="card.id">
+            <Card v-bind="card"></Card>
+          </div>
+        </draggable>
+      </div>
+    </div>
+    <!-- END SELF BATTLEFIELD -->
 
-        <!-- SELF - BATTLEFIELD -->
-        <div class="column">
-          <p class="title is-5">Battlefield</p>
-          <draggable
-            class="card-wrapper"
-            @change="handleChange()"
-            v-model="self.Field"
-            group="people"
-            @start="drag = true"
-            @end="drag = false"
-          >
-            <div v-for="card in self.Field" :key="card.id" class="columns">
-              <Card v-bind="card" />
-            </div>
-          </draggable>
-        </div>
-        <!-- END SELF BATTLEFIELD -->
-
-        <!-- SELF - LIBRARY  -->
-        <div class="column" @click="handleDraw()">
-          <p class="title is-5">Library</p>
-          <draggable
-            class="column card-wrapper"
-            v-model="self.Library"
-            group="people"
-            @change="handleChange()"
-            @start="drag = true"
-            @end="drag = false"
-          >
-            <div v-for="card in self.Library" :key="card.id">
-              <Card v-bind="card" hidden="true" />
-            </div>
-          </draggable>
-        </div>
-
-        <div class="column">
-          <p class="title is-4">Hand</p>
-          <draggable
-            class="columns card-wrapper"
-            v-model="self.Hand"
-            group="people"
-            @change="handleChange()"
-            @start="drag = true"
-            @end="drag = false"
-          >
-            <div class="column mtg-card" v-for="card in self.Hand" :key="card.id">
-              <Card v-bind="card"></Card>
-            </div>
-          </draggable>
+    <!-- SELF - PRIVATE SECTION -->
+    <div class="columns">
+      <div class="column">
+        <div class="columns is-mobile is-multiline">
+          <div class="column is-full">
+            <!-- SELF - HAND-->
+            <p class="title is-5">Hand</p>
+            <draggable
+              class="columns is-flex is-multiline is-mobile is-align-items-flex-start"
+              v-model="self.Hand"
+              group="people"
+              @change="handleChange()"
+              @start="drag = true"
+              @end="drag = false"
+            >
+              <div class="column mtg-card" v-for="card in self.Hand" :key="card.id">
+                <Card v-bind="card"></Card>
+              </div>
+            </draggable>
+          </div>
         </div>
       </div>
-
-      <!-- ### END OF SELF ### -->
     </div>
+
+    <hr />
+
+    <template>
+      <b-navbar>
+        <template #start>
+            <draggable
+              v-model="self.Library"
+              group="people"
+              @change="handleChange()"
+              @start="drag = true"
+              @end="drag = false"
+            >
+          <b-navbar-item @click="handleDraw()" href="#">
+              <button class="button is-primary">
+                <span class="icon">
+                  <i class="fa fa-book"></i>
+                </span>
+                <span>Draw</span>
+              </button>
+          </b-navbar-item>
+            </draggable>
+          <b-navbar-item href="#"> 
+            <button class="button is-primary">
+              <span class="icon">
+                <i class="fa fa-arrow-up"></i>
+              </span>
+              <span>Untap</span>
+            </button>
+          </b-navbar-item>
+          <!-- <b-navbar-dropdown label="Info">
+              <b-navbar-item href="#"> About </b-navbar-item>
+              <b-navbar-item href="#"> Contact </b-navbar-item>
+            </b-navbar-dropdown> -->
+        </template>
+
+        <template #end>
+          <b-navbar-item tag="div">
+            <div class="buttons">
+              <!-- <a class="button is-primary"> <strong>Sign up</strong> </a>
+              <a class="button is-light"> Log in </a> -->
+            </div>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+    </template>
+  </div>
+</template>
+
+
+
+
+
+    <!-- END OF SELF PRIVATE VIEW -->
+    <!-- ### END OF SELF ### -->
   </div>
 </template>
 <script>
@@ -96,9 +138,6 @@ export default {
       this.$store.dispatch('subscribeToGame', this.$route.params.id);
       this.$store.dispatch('getBoardStates', this.$route.params.id);
     });
-
-    // get initial boardstates
-    console.log('getting game ', this.$route.params.id);
   },
   computed: mapState({
     game: (state) => state.Game.game,
@@ -107,13 +146,15 @@ export default {
     user: (state) => state.User.User,
   }),
   methods: {
-    handleIncLife() {},
-    handleDecLife() {},
     handleDraw() {
       this.$store.dispatch('draw', this.self);
     },
     handleChange() {
       this.$store.dispatch('mutateBoardState', this.self);
+    },
+    handleTap(card) {
+      card.Tapped = true;
+      this.handleChange();
     },
   },
   components: {
