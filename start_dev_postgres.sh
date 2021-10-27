5d6859f0107ce8e3554e98aeb70c12596cb40466
dylan_lott <lott.dylan@gmail.com>
2021-10-26 23:25:02 -0600

    moves start script

--- start_dev_postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this script starts a database for local dev.
-docker run -d \
-    -e POSTGRES_PASSWORD=edhgodev \
-    -e POSTGRES_USER=edhgo \
-    -e POSTGRES_DB=edhgo \
-    -p 5432:5432 \
-    postgres
