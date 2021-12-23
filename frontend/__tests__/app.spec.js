51e53486ed1ff285376f7ae07b1a4f8c89174b43
dylan_lott <lott.dylan@gmail.com>
2021-12-22 18:04:28 -0700

    removes dead test code

--- frontend/__tests__/app.spec.js
+++ frontend/__tests__/app.spec.js
@@ -7,33 +7,6 @@ import store from '../src/store';
 import { isType } from 'graphql';
 
 describe('edhgo', () => {
-    // const wrapper = createLocalVue()
-
-    // test('does a wrapper exist', () => {
-    //   expect(wrapper.exists()).toBe(true)
-    // })
-
-    // let wrp;
-    // const routes = [{ path: '/games/:id', name: 'board' }]
-    // const router = new VueRouter({ routes })
-    // beforeEach(() => {
-    //     const localVue = createLocalVue()
-    //     // use our framework, router, and store for tests
-    //     localVue.use(VueRouter)
-    //     localVue.use(Buefy)
-    //     localVue.use(Vuex)
-
-    //     wrp = mount(Board, {
-    //         localVue: localVue,
-    //         // add on our own store and router 
-    //         store,
-    //         router,
-    //     })
-    // })
-
-    // it('should have an App', (t) => {
-    //     expect(wrp.exists()).toBe(true)
-    // })
     it('renders an app', () => {
         const wrapper = shallowMount(App)
         expect(wrapper.contains('div')).toBe(true)
