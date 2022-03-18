66d2ec1152fec3a54094a46bf9fca032d0290072
shakezula <lott.dylan@gmail.com>
2022-03-18 01:01:43 -0600

    landing page updates

--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -7,7 +7,7 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title is-1 has-text-centered">
-            Play Commander games on the go with EDH Go.
+            Play Commander on the web with EDH Go.
           </h1>
           <div class="subtitle">
               <!-- Begin Mailchimp Signup Form -->
@@ -22,7 +22,7 @@
                   novalidate>
                   <div id="mc_embed_signup_scroll">
                   	<label for="mce-EMAIL">
-                      <p class="subtitle">Sign up now and get early access when we launch the Beta!</p>
+                      <p class="subtitle has-text-centered">Sign up now and get early access when we launch.</p>
                     </label>
                     <br>
                   	<b-input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required/>
@@ -30,7 +30,7 @@
                       <div style="position: absolute; left: -5000px;" aria-hidden="true">
                         <input type="text" name="b_2c6b66f6b94ee6dfbf9524986_36cf9e0d6c" tabindex="-1" value="">
                       </div>
-                      <div class="clear">
+                      <div align="center">
                         <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button subscribe-button is-success is-medium is-block">
                       </div>
                     </div>
@@ -41,91 +41,57 @@
         </div>
       </div>
     </section>
+
     <!-- Benefits Tiles -->
     <section class="columns benefits">
       <div class="column">
-        <article class="tile is-child notification is-white box has-text-centered">
-          <h3><i class="fa fa-mobile fa-3x"></i></h3>
+        <article class="tile is-child notification is-white has-text-centered">
+          <h3><i class="fa fa-mobile-alt fa-3x"></i></h3>
           <p class="title">Mobile First</p>
           <p class="subtitle">Play anytime, anywhere, on any device with an internet connection and a browser.</p>
         </article>
       </div>
       <div class="column">
-        <article class="tile is-child notification is-white box has-text-centered">
+        <article class="tile is-child notification is-white has-text-centered">
           <h3><i class="fa fa-arrows-alt fa-3x"></i></h3>
           <p class="title">Flexible</p>
           <p class="subtitle">Our boardstate engine allows for any format to be played, with a focus on EDH games.</p>
         </article>
       </div>
       <div class="column">
-        <article class="tile is-child notification is-white box has-text-centered">
-          <h3><i class="fa fa-star fa-3x"></i></h3>
+        <article class="tile is-child notification is-white has-text-centered">
+          <h3><i class="fa fa-vial fa-3x"></i></h3>
           <p class="title">Simple</p>
           <p class="subtitle">We worked hard to keep the interface intuitive and easy to use on mobile <i>and</i> desktop.</p>
         </article>
       </div>
       <div class="column">
-        <article class="tile is-child notification is-white box has-text-centered">
+        <article class="tile is-child notification is-white has-text-centered">
           <h3><i class="fa fa-tachometer-alt fa-3x"></i></h3>
           <p class="title">Fast</p>
           <p class="subtitle">The app is built from the ground up from scratch to be as small, fast, and reliable as possible.</p>
         </article>
       </div>
     </section>
-    <section class="columns level screenshot">
-      <div class="column">
-        <article class="box levels">
-          <h1 class="title is-1 level-item">
-          EDH-Go is the first Magic: The Gathering board state tracker of its kind.
-          </h1>
-          <a target="_blank" href="https://i.imgur.com/1YUGzki.png">
-            <img class="level-item screen" src="https://i.imgur.com/1YUGzki.png" alt="Magic: The Gathering Board State Tracker">
-          </a>
-          <p class="level-item what-is-edhgo">
-            <h1 class="title is-2 has-text-centered">What is EDH-Go?</h1>
-            <p class="subtitle has-text-centered">
-              EDH-Go is a boardstate tracker that puts the power to control the game in the players hands.
-              We don't try to handle all the logic of every single card interaction in the game.
-              Instead, we leave that up to the players and focus on tracking those changes in the cleanest, most efficient way possible.
-              This allows us to serve mobile users with a robust platform that's still performant even on limited internet connections.
-            </p>
-            <p class="level-item">
-              <h1 class="title is-2 has-text-centered">The EDH Social Contract</h1>
-              <p class="subtitle has-text-centered">
-                We rely on the EDH Social Contract. This means we make a few pragmatic tradeoffs to achieve our end goal of performance on any platform.
-                Player actions are auditable and visible to all other opponents, but their values are not. For example, drawing a card will be visible to
-                all players, but what you drew is not visible. Anything that you'd be able to see a player do at the table, you'll be able to tell if a player
-                did that in EDH-Go. 
-              </p>
-              <p class="subtitle has-text-centered">
-                This works because we fundamentally believe in people's ability to self-monitor and be honest and transparent. If you want to cheat,
-                everyone will see it and you're only playin' yourself.
-                <br>
 
-              </p>
-              <h1 class="title is-1 has-text-centered">
-                <b>Be a good player, be a good person.</b>
-              </h1>
-            </p>
-          </p>
-        </article>
-      </div>
-    </section>
     <!-- Footer -->
     <footer class="footer">
       <div class="content has-text-centered">
-        <p><strong>EDH-Go</strong> by <a href="https://dylanlott.com/">Hivemind.</a></p>
+        <p><strong>EDH-Go</strong> powered by Stamina Crü Software.</p>
+        <p class="text is-size-7">Build v{{ version }} </p>
       </div>
     </footer>
   </div>
 </div>
 </template>
 <script type="text/javascript">
+import pkg from '../../package.json'
 export default {
   name: 'landing',
   data () {
     return {
-      year: 2020
+      year: new Date().getFullYear().toString(),
+      version: pkg.version,
     }
   }
 }
@@ -135,7 +101,7 @@ export default {
 
 #mc_embed_signup{
   clear:left; 
-  font:14px Helvetica,Arial,sans-serif; 
+  font:14px Inter,Arial,sans-serif; 
   width:100%;
 }
 .signup-button {
