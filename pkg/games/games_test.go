e84ea2e3b81cbcc61b6d326c41157f3a74003636
shakezula <lott.dylan@gmail.com>
2022-07-09 17:46:45 -0600

    WIP breaks out player into separate interface
    * creates the Player interface
    * Player is meant to be a small wrapper around a JSON type
    * JSON type is our current base structure of data for extensibility

--- pkg/games/games_test.go
+++ pkg/games/games_test.go
@@ -16,15 +16,13 @@ func TestInMemoryGame(t *testing.T) {
 		}
 
 		now := time.Now()
-		created, err := m.NewFullGame("test", []Player{
-			&player{boardstate: make(JSON, 0)},
-		})
+		created, err := m.NewFullGame("test", []Player{})
 		is.NoErr(err)
 		is.True(len(m.games) > 0)
 		is.True(created.createdAt.After(now))
 	})
 
-	t.Run("should return a game", func(t *testing.T) {
+	t.Run("should return a list of game", func(t *testing.T) {
 		m := &MemStore{
 			games: map[string]Game{
 				"foo": &FullGame{
@@ -38,5 +36,47 @@ func TestInMemoryGame(t *testing.T) {
 		games, err := m.List()
 		is.NoErr(err)
 		is.True(len(games) == 1)
+	})
+
+	t.Run("should update a player's boardstate", func(t *testing.T) {
+		m := &MemStore{
+			games: map[string]Game{
+				"foo": &FullGame{
+					id: "foo",
+					players: []Player{
+						&inMemPlayer{
+							state: map[string]interface{}{
+								"id": "bar",
+							},
+						},
+					},
+					createdAt: time.Time{},
+				},
+			},
+		}
+
+		// get game foo
+		g, err := m.Get("foo")
+		is.NoErr(err)
+
+		// get player bar
+		p, err := g.Get("bar")
+		is.NoErr(err)
+
+		// get player's boardstate
+		bs, err := p.Boardstate()
+		is.NoErr(err)
+
+		// mutate a piece of state
+		bs["biz"] = "baz"
+
+		// update player
+		err = p.Sync(bs)
+		is.NoErr(err)
+
+		got, err := p.Boardstate()
+		is.NoErr(err)
+		is.Equal(got["biz"], "baz")
+		// TODO: assert that events are emitted through PubSub
 	})
 }
