2fa1c8388e4af98222ef92a1b8410bdfe09be36a
shakezula <lott.dylan@gmail.com>
2022-07-09 17:46:45 -0600

    WIP breaks out player into separate interface
    * creates the Player interface
    * Player is meant to be a small wrapper around a JSON type
    * JSON type is our current base structure of data for extensibility

--- pkg/games/games.go
+++ pkg/games/games.go
@@ -8,10 +8,13 @@ import (
 	"github.com/google/uuid"
 )
 
+// Games are managed by the GameService. Once a Game is acquired,
+// all updates and state management is done through the Game API.
+
 // GameService defines a CRUD interface for Games.
 type GameService interface {
 	Create(game Game) (Game, error)
-	Update(game Game) (Game, error)
+	Join(gameID string, player Player) (Game, error)
 	List() ([]Game, error)
 	Get(id string) (Game, error)
 }
@@ -19,59 +22,87 @@ type GameService interface {
 // JSON is a shortcut type for dealing with JSON
 type JSON map[string]interface{}
 
-// Players have a Boardstate and an ID.
+//
+// PLAYER
+//
+
+// Player is essentially a wrapper around an arbitrary JSON
+// object.
 type Player interface {
 	ID() string
 	Boardstate() (JSON, error)
+	Sync(JSON) error
 }
 
-type player struct {
-	boardstate JSON
+type inMemPlayer struct {
+	sync.Mutex
+
+	state JSON
 }
 
-func (p *player) ID() string {
-	if p.boardstate == nil {
-		p.boardstate = make(JSON)
+func (p *inMemPlayer) ID() string {
+	if p.state == nil {
+		p.state = make(JSON)
 	}
 
-	if val, ok := p.boardstate["id"]; ok {
+	if val, ok := p.state["id"]; ok {
 		return fmt.Sprintf("%v", val)
 	}
 
 	return "<ErrNoId>"
 }
 
-func (p *player) Boardstate() (JSON, error) {
-	if p.boardstate == nil {
-		p.boardstate = make(JSON)
+// Boardstate returns the current boardstate for the Player.
+func (p *inMemPlayer) Boardstate() (JSON, error) {
+	if p.state == nil {
+		p.state = make(JSON)
 	}
-	return p.boardstate, nil
+	return p.state, nil
 }
 
-type Card interface {
-	ID() string
-	Name() string
-	Data() JSON
+// Sync
+func (p *inMemPlayer) Sync(json JSON) error {
+	p.Lock()
+	defer p.Unlock()
+
+	p.state = json
+	return nil
 }
+
+//
+// GAME
+//
 
 // Game declares the interface to our main resource: games
 type Game interface {
 	// All games must have a unique ID
 	ID() string
-	// Games are made up of player Boardstates in a specific order.
-	// Turn order is described by the order of Boardstates returned by Players.
+
+	// Players returns a list of players in the Game.
+	// * Games are made up of player Boardstates in a specific order.
+	// * Turn order is described by the order of Boardstates returned by Players.
 	Players() ([]Player, error)
-	// Games have a Pub/Sub model built into them for realtime
+
+	// Sync allows a Game to update n number of Players and updates
+	// its internal view of those Players to match.
+	// * If a Player can't be found by ID in the Game, it will error.
+	// * Sync emits an update through PubSub every time a Player is successfully
+	// updated.
+	// * A failed Sync will not emit any events.
+	// Sync(players ...Player) ([]Player, error)
+
+	// Join will join a Player to the Game that matches `gameID`
+	Join(gameID string, player Player) (Game, error)
+
+	// Get returns a single Player that matches `playerID` from the Game.
+	// If no player exists with that ID it will return an error.
+	Get(playerID string) (Player, error)
+	// Games have a Pub/Sub model built into them for realtime notifications
+	// of their entire view.
 	PubSub
 }
 
-// PubSub declares a generic pub/sub interface for any type
-type PubSub interface {
-	Subscribe() (<-chan Game, error)
-	Publish(game Game) error
-}
-
-// GQL is the eventual target for our GraphQL API to fulfill.
+// TODO: make GQL a collection of FullGames served by GQL.
 type GQL struct{} // TODO
 
 // FullGame is an inmemory game store for testing and validation purposes
@@ -79,12 +110,15 @@ type FullGame struct {
 	id        string
 	players   []Player
 	createdAt time.Time
+
+	// pub       chan Game // TODO: implement pub sub
 }
 
 // MemStore fulfills the GameService interface and creates a API for
 // managing multiple Games.
 type MemStore struct {
 	sync.Mutex
+
 	games map[string]Game
 }
 
@@ -107,13 +141,22 @@ func (m *MemStore) NewFullGame(id string, players []Player) (*FullGame, error) {
 	m.games[g.ID()] = g
 	return g, nil
 }
-func (m *MemStore) Update(game Game) (Game, error) {
+
+func (m *MemStore) Join(gameID string, player Player) (Game, error) {
+	if gameID == "" {
+		return nil, fmt.Errorf("ErrNoGameID")
+	}
+
 	return nil, fmt.Errorf("not impl")
 }
+
+// List returns a list of Games that are in the store currently.
 func (m *MemStore) List() ([]Game, error) {
 	m.Lock()
 	defer m.Unlock()
 
+	// TODO: return only active games.
+	// TODO: sort and filter this list before returning
 	list := []Game{}
 	for _, v := range m.games {
 		list = append(list, v)
@@ -121,19 +164,50 @@ func (m *MemStore) List() ([]Game, error) {
 
 	return list, nil
 }
+
+// Get returns the Game that matches `id` or an error.
 func (m *MemStore) Get(id string) (Game, error) {
-	return nil, fmt.Errorf("not impl")
+	if game, ok := m.games[id]; ok {
+		return game, nil
+	}
+
+	return nil, fmt.Errorf("ErrNoExist: %s", id)
 }
 
 // All games must have a unique ID
-func (i *FullGame) ID() string {
-	return i.id
+func (f *FullGame) ID() string {
+	return f.id
 }
 
 // Games are made up of player Boardstates in a specific order.
 // Turn order is described by the order of Boardstates returned by Players.
-func (i *FullGame) Players() ([]Player, error) {
-	panic("not implemented") // TODO: Implement
+func (f *FullGame) Players() ([]Player, error) {
+	return f.players, nil
+}
+
+// Get returns a single Player from the full game.
+func (f *FullGame) Get(playerID string) (Player, error) {
+	for _, player := range f.players {
+		if player.ID() == playerID {
+			return player, nil
+		}
+	}
+	return nil, fmt.Errorf("ErrPlayerNotExist: id %s not found", playerID)
+}
+
+// Join will add a Player to a Game or return an error.
+func (f *FullGame) Join(gameID string, player Player) (Game, error) {
+	return nil, fmt.Errorf("not impl")
+}
+
+//
+//PUBSUB
+//
+
+// PubSub declares a generic pub/sub interface for any type
+type PubSub interface {
+	Subscribe() (<-chan Game, error)
+	Publish(game Game) error
 }
 
 // Subscribe emits any changes to the FullGame
