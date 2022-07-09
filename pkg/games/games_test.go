740024ccbb7b63ddb45800f689b919badabbcd84
shakezula <lott.dylan@gmail.com>
2022-07-08 21:55:26 -0600

    WIP adds list test

--- pkg/games/games_test.go
+++ pkg/games/games_test.go
@@ -23,4 +23,20 @@ func TestInMemoryGame(t *testing.T) {
 		is.True(len(m.games) > 0)
 		is.True(created.createdAt.After(now))
 	})
+
+	t.Run("should return a game", func(t *testing.T) {
+		m := &MemStore{
+			games: map[string]Game{
+				"foo": &FullGame{
+					id:        "foo",
+					players:   []Player{},
+					createdAt: time.Time{},
+				},
+			},
+		}
+
+		games, err := m.List()
+		is.NoErr(err)
+		is.True(len(games) == 1)
+	})
 }
