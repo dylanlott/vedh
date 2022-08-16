831bd29baf30b34a5e25b620a96fc5b94568cee6
shakezula <lott.dylan@gmail.com>
2022-08-16 11:57:45 -0600

    wip: distinct drag and drop zones for hand and battlefield

--- frontend/src/components/DraggableCard.vue
+++ frontend/src/components/DraggableCard.vue
@@ -1,89 +1,78 @@
 <template>
-    <div ref="myDraggable" class="draggable">
-      <!--  Wrap Card here -->
-      <Card v-bind="card"/>
-    </div>
+  <div ref="myDraggable" class="draggable">
+    <!--  Wrap Card here -->
+    <Card v-bind="card" />
+  </div>
 </template>
 
 <script>
 import interact from 'interactjs';
 import Card from '@/components/Card';
 export default {
-  name: "Draggable",
+  name: 'DraggableCard',
   data() {
     return {
       screenX: 0,
-      screenY: 0
+      screenY: 0,
     };
   },
   props: {
     card: Object,
   },
-  mounted: function() {
+  mounted: function () {
     let myDraggable = this.$refs.myDraggable;
     this.initInteract(myDraggable);
   },
   methods: {
-    initInteract: function(selector) {
+    initInteract: function (selector) {
       interact(selector).draggable({
         // enable inertial throwing
         inertia: true,
         // keep the element within the area of it's parent
-        restrict: {
-          restriction: "parent",
-          endOnly: true,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
+        // restrict: {
+          // restriction: 'parent',
+          // endOnly: true,
+          // elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+        // },
         // enable autoScroll
         autoScroll: true,
 
         // call this function on every dragmove event
         onmove: this.dragMoveListener,
         // call this function on every dragend event
-        onend: this.onDragEnd
+        onend: this.onDragEnd,
       });
     },
-    dragMoveListener: function(event) {
+    dragMoveListener: function (event) {
       var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
-        x =
-          (parseFloat(target.getAttribute("data-x")) || this.screenX) +
-          event.dx,
-        y =
-          (parseFloat(target.getAttribute("data-y")) || this.screenY) +
-          event.dy;
+        x = (parseFloat(target.getAttribute('data-x')) || this.screenX) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || this.screenY) + event.dy;
 
       // translate the element
-      target.style.webkitTransform = target.style.transform =
-        "translate(" + x + "px, " + y + "px)";
+      target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 
       // update the posiion attributes
-      target.setAttribute("data-x", x);
-      target.setAttribute("data-y", y);
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
     },
-    onDragEnd: function(event) {
+    onDragEnd: function (event) {
       var target = event.target;
-      console.log(target);
       // update the state
       this.screenX = target.getBoundingClientRect().left;
       this.screenY = target.getBoundingClientRect().top;
-    }
+    },
   },
   components: {
     Card,
-  }
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .draggable {
-  width: 100px;
-  height: 100px;
-  background-color: teal;
-  color: #fff;
   padding: 5px;
   position: absolute;
 }
 </style>
-
