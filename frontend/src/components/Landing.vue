9cab92dcea22d8bf11309ab182e9d6fdae465776
shakezula <lott.dylan@gmail.com>
2023-10-25 22:39:16 -0600

    landing page updates

--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -1,131 +1,142 @@
 <template>
-<div>
-  <!-- Page Content -->
-  <div class="shell landing">
-    <!-- Jumbotron Header -->
-    <section class="hero is-medium is-primary is-bold welcome">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-1 has-text-centered">
-            Play Commander on the web with EDH Go.
-          </h1>
-          <div class="subtitle">
-              <!-- Begin Mailchimp Signup Form -->
-              <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-              <div id="mc_embed_signup">
-                <form action="https://gmail.us18.list-manage.com/subscribe/post?u=2c6b66f6b94ee6dfbf9524986&amp;id=36cf9e0d6c"
-                  method="post"
-                  id="mc-embedded-subscribe-form"
-                  name="mc-embedded-subscribe-form"
-                  class="validate"
-                  target="_blank"
-                  novalidate>
-                  <div id="mc_embed_signup_scroll">
-                  	<label for="mce-EMAIL">
-                      <p class="subtitle has-text-centered">Sign up now and get early access when we launch.</p>
-                    </label>
-                    <br>
-                  	<b-input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required/>
-                      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                      <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                        <input type="text" name="b_2c6b66f6b94ee6dfbf9524986_36cf9e0d6c" tabindex="-1" value="">
-                      </div>
-                      <div align="center">
-                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button subscribe-button is-success is-medium is-block">
-                      </div>
-                    </div>
-                  </form>
+  <div>
+    <!-- Page Content -->
+    <div class="shell landing">
+      <!-- Jumbotron Header -->
+      <section class="hero is-medium is-primary is-bold welcome">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title is-1 has-text-centered">
+              Sign up below to play Commander with EDH Go.
+            </h1>
+            <div class="columns is-centered">
+              <div class="column is-4 is-mobile">
+                <div class="box">
+                  <b-field label="Username" :label-position="labelPosition">
+                    <b-input v-model="username"></b-input>
+                  </b-field>
+                  <b-field v-on:keyup.enter="handleSignup()" @submit="handleSignup()" label="Password"
+                    :label-position="labelPosition">
+                    <b-input type="password" v-model="password"></b-input>
+                  </b-field>
+                  <b-button @submit="handleSignup()" @click="handleSignup()" v-on:keyup.enter="handleSignup()"
+                    type="is-primary">
+                    Sign Up
+                  </b-button>
+                  <div class="already-a-member">
+                    Already a member? <a href="/login">Login.</a>
+                  </div>
                 </div>
-              <!--End mc_embed_signup-->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Benefits Tiles -->
+      <section class="columns benefits">
+        <div class="column">
+          <article class="tile is-child notification is-white has-text-centered">
+            <h3><i class="fa fa-mobile-alt fa-3x"></i></h3>
+            <p class="title">Mobile First</p>
+            <p class="subtitle">Play anytime, anywhere, on any device with an internet connection and a browser.</p>
+          </article>
+        </div>
+        <div class="column">
+          <article class="tile is-child notification is-white has-text-centered">
+            <h3><i class="fa fa-arrows-alt fa-3x"></i></h3>
+            <p class="title">Flexible</p>
+            <p class="subtitle">Our boardstate engine allows for any format to be played, with a focus on EDH games.</p>
+          </article>
+        </div>
+        <div class="column">
+          <article class="tile is-child notification is-white has-text-centered">
+            <h3><i class="fa fa-vial fa-3x"></i></h3>
+            <p class="title">Simple</p>
+            <p class="subtitle">We worked hard to keep the interface intuitive and easy to use on mobile <i>and</i>
+              desktop.</p>
+          </article>
+        </div>
+        <div class="column">
+          <article class="tile is-child notification is-white has-text-centered">
+            <h3><i class="fa fa-tachometer-alt fa-3x"></i></h3>
+            <p class="title">Fast</p>
+            <p class="subtitle">The app is built from the ground up from scratch to be as small, fast, and reliable as
+              possible.</p>
+          </article>
+        </div>
+      </section>
+
+      <!-- Mailchimp signup -->
+      <div class="columns is-centered">
+        <div class="box">
+          <div class="column is-4 is-mobile">
+            <Mailchimp />
           </div>
         </div>
       </div>
-    </section>
 
-    <!-- Benefits Tiles -->
-    <section class="columns benefits">
-      <div class="column">
-        <article class="tile is-child notification is-white has-text-centered">
-          <h3><i class="fa fa-mobile-alt fa-3x"></i></h3>
-          <p class="title">Mobile First</p>
-          <p class="subtitle">Play anytime, anywhere, on any device with an internet connection and a browser.</p>
-        </article>
-      </div>
-      <div class="column">
-        <article class="tile is-child notification is-white has-text-centered">
-          <h3><i class="fa fa-arrows-alt fa-3x"></i></h3>
-          <p class="title">Flexible</p>
-          <p class="subtitle">Our boardstate engine allows for any format to be played, with a focus on EDH games.</p>
-        </article>
-      </div>
-      <div class="column">
-        <article class="tile is-child notification is-white has-text-centered">
-          <h3><i class="fa fa-vial fa-3x"></i></h3>
-          <p class="title">Simple</p>
-          <p class="subtitle">We worked hard to keep the interface intuitive and easy to use on mobile <i>and</i> desktop.</p>
-        </article>
-      </div>
-      <div class="column">
-        <article class="tile is-child notification is-white has-text-centered">
-          <h3><i class="fa fa-tachometer-alt fa-3x"></i></h3>
-          <p class="title">Fast</p>
-          <p class="subtitle">The app is built from the ground up from scratch to be as small, fast, and reliable as possible.</p>
-        </article>
-      </div>
-    </section>
 
-    <!-- Footer -->
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p class="text is-size-7"><strong>EDHGo</strong> powered by the Hivemind.
-        <p class="text is-size-7">Build v{{ version }} </p>
-      </div>
-    </footer>
+      <!-- Footer -->
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p class="text is-size-7"><strong>EDHGo</strong> powered by the Hivemind.</p>
+          <p class="text is-size-7">Build v{{ version }} </p>
+        </div>
+      </footer>
+    </div>
   </div>
-</div>
 </template>
 <script type="text/javascript">
 import pkg from '../../package.json'
+import Mailchimp from './Mailchimp.vue';
+import Signup from './Signup.vue';
 export default {
   name: 'landing',
-  data () {
+  data() {
     return {
+      username: "",
+      password: "",
       year: new Date().getFullYear().toString(),
       version: pkg.version,
-    }
-  }
+      labelPosition: "centered"
+    };
+  },
+  components: { Signup, Mailchimp }
 }
 </script>
 <style type="text/css" lang="scss" scoped>
 @import "@/scss/custom.scss";
 
-#mc_embed_signup{
-  clear:left; 
-  font:14px Inter,Arial,sans-serif; 
-  width:100%;
-}
 .signup-button {
   margin: .75rem 0rem;
 }
+
 .benefits {
   margin: 1rem -.7rem;
 }
+
 .subscribe-button {
   margin: 1rem 0rem;
 }
+
 .footer {
   width: 100%;
 }
+
 .benefits {
   margin: 2rem;
 }
+
 .screenshot {
   margin: .25rem;
 }
+
 .screen {
   border: solid 2px $primary;
   border-radius: 5px;
 }
+
 .what-is-edhgo {
   margin: 1rem;
 }
