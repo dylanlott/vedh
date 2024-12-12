9a41c509a94d191a8cddde7746f65da69e656664
Dylan Lott <lott.dylan@gmail.com>
2024-12-12 11:53:29 -0700

    update sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 REMOTE_USER="root"
-REMOTE_HOST="www.edhgo.com"
-REMOTE_DIR="/home/shakezula/edh-go"
+REMOTE_HOST="192.241.142.53"
+REMOTE_DIR="/root/vedh/"
 
 # rsync -avz --progress ./ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 git ls-files | rsync -av --progress --files-from=- ./ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 
 echo ""
 echo "↘️↖️ Sync completed ✅"
+
+# raw script:
+# rsync -avz --exclude-from=<(git ls-files --others --exclude-standard --ignored -o --directory) ./ root@192.241.142.53:/root/vedh/
