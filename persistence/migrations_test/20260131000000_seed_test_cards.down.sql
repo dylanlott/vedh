c48353e478da238ab9b554f3bb3eb1861de51eac
Dylan Lott <lott.dylan@gmail.com>
2026-01-30 23:42:54 -0700

    fix(migrations): drop cards with cascade in down

--- persistence/migrations_test/20260131000000_seed_test_cards.down.sql
+++ persistence/migrations_test/20260131000000_seed_test_cards.down.sql
@@ -3,3 +3,4 @@ DELETE FROM cards WHERE uuid LIKE 'testseed-%';
 DROP INDEX IF EXISTS allcards_facename_idx;
 DROP INDEX IF EXISTS allcards_name_idx;
 DROP TABLE IF EXISTS allcards;
+DROP TABLE IF EXISTS cards CASCADE;
