3dc2aef05138ac83ef8a2102c43e37af271e5640
Dylan Lott <lott.dylan@gmail.com>
2026-01-30 23:48:40 -0700

    test: reset db migrations before server tests

--- server/main_test.go
+++ server/main_test.go
@@ -9,12 +9,21 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	code := m.Run()
-
 	dbURL := os.Getenv("DATABASE_URL")
 	if dbURL == "" {
 		dbURL = "postgres://edhgo:edhgo@localhost:5432/edhgo?sslmode=disable&connect_timeout=3"
 	}
+	if err := persistence.ForceCleanMigrations("../persistence/migrations_test/", dbURL); err != nil {
+		fmt.Fprintf(os.Stderr, "test setup failed: %v\n", err)
+		os.Exit(1)
+	}
+	if err := persistence.MigrateDown("../persistence/migrations_test/", dbURL); err != nil {
+		fmt.Fprintf(os.Stderr, "test setup failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	code := m.Run()
+
 	if err := persistence.MigrateDown("../persistence/migrations_test/", dbURL); err != nil {
 		fmt.Fprintf(os.Stderr, "test cleanup failed: %v\n", err)
 		if code == 0 {
